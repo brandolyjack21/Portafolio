e1f49fe8d5c4d511026c360ffaf79a3c1837d2c3
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2023-10-25 15:43:12 -0500

    portafolio mobile first about portafolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,23 +10,13 @@ function About(){
                     </section>
                     <section className='container-description-profesional'>
                         <section className='profesional-description'>
-                            <p>Soy un apasionado programador con un enfoque en el desarrollo de sitios web utilizando WordPress y la potente herramienta de diseño, Elementor. Mi amor por el mundo del SEO me llevó a aprender JavaScript y React, junto con Node.js y Express, para crear experiencias web altamente interactivas y optimizadas para motores de búsqueda.</p>
+                            <p>"Soy un apasionado programador web con experiencia en tecnologías como <strong>JavaScript, React, Redux, Node.js y PostgreSQL</strong>.<br/><br/>
+
+Diseño soluciones web innovadoras y elegantes que impulsan el éxito de tu negocio. Si buscas resultados excepcionales, ¡contáctame hoy!"</p>
                         </section>
                         <section className='container-img-and-skils'>
                             <div className='img-about'>
                                 <img src={imgAbout} alt="" />
-                            </div>
-                            <div className='description-skils'>
-                                <div className='container-habilidades'>
-                                    <h4>Habilidades usadas en mis proyectos</h4>
-                                </div>
-                                <ul>
-                                    <li className='list-habilidades'><i class='bx bx-chevron-right'></i>Javascript</li>
-                                    <li className='list-habilidades'><i class='bx bx-chevron-right'></i>Html</li>
-                                    <li className='list-habilidades'><i class='bx bx-chevron-right'></i>Css</li>
-                                    <li className='list-habilidades'><i class='bx bx-chevron-right'></i>React</li>
-                                    <li className='list-habilidades'><i class='bx bx-chevron-right'></i>php</li>
-                                </ul>
                             </div>
                         </section>
                     </section>
