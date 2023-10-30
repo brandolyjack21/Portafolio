a147142a081891c7418bd0c35dccb699e06221f2
brandoly-jack <143424064+brandoly-jack@users.noreply.github.com>
2023-10-30 11:39:26 -0500

    preparado para dispositivos moviles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,73 @@
-import imgAbout from '../assets/fotodeperfil.jpeg'
+import imgAbout from "../assets/fotodeperfil.jpeg";
 
-function About(){
-    return(
-        <>
-            <section className='container-about'>
-                <article className='body-about'>
-                    <section className='container-h2'>
-                        <h2>Acerca de Mí</h2>
+function About() {
+  const projects = [
+    {
+      name: "Ecommerce",
+      technologies: ["HTML ","CSS ","JavaScript ","React ","Redux ","React-Router ","Bootstrap "],
+      description:"Un sitio web de ecommerce que permite a los usuarios comprar productos de forma online de una manera intuitiva y fácil de usar.",
+    },
+    {
+      name: "Clima",
+      technologies: ["React ", "HTML ", "JavaScript ", "API REST "],
+      description:
+        "Una aplicación web que muestra la temperatura actual en tu ubicación y también permite buscar la temperatura de cualquier ciudad del mundo.",
+    },
+    {
+      name: "Pokeapi",
+      technologies: ["React ", "HTML ", "JavaScript ", "API REST "],
+      description:
+        "Una aplicación web que muestra los diferentes tipos de Pokémon.",
+    },
+  ];
+  return (
+    <>
+      <section className="container-about">
+        <article className="body-about">
+          <section className="container-h2">
+            <h2>Acerca de Mí</h2>
+          </section>
+          <section className="container-description-profesional">
+            <section className="profesional-description">
+              <p className="description">
+                Soy un apasionado programador web con experiencia en tecnologías
+                como{" "}
+                <strong>JavaScript, React, Redux, Node.js y PostgreSQL</strong>.
+                <br />
+                <br />
+                Diseño soluciones web innovadoras y elegantes que impulsan el
+                éxito de tu negocio. Si buscas resultados excepcionales,
+                ¡contáctame hoy!
+              </p>
+              <h4 className="title-presentation-project">Estos son algunos de mis proyectos:</h4>
+              <ul className="container-list-description-projects">
+                {projects.map((project) => (
+                  <li className="description-project">
+                    <section className="container-line-circle">
+                      <div className="line"></div>
+                      <div className="circle"></div>
                     </section>
-                    <section className='container-description-profesional'>
-                        <section className='profesional-description'>
-                            <p>"Soy un apasionado programador web con experiencia en tecnologías como <strong>JavaScript, React, Redux, Node.js y PostgreSQL</strong>.<br/><br/>
-
-Diseño soluciones web innovadoras y elegantes que impulsan el éxito de tu negocio. Si buscas resultados excepcionales, ¡contáctame hoy!"</p>
-                        </section>
-                        <section className='container-img-and-skils'>
-                            <div className='img-about'>
-                                <img src={imgAbout} alt="" />
-                            </div>
-                        </section>
+                    <section className="project-description">
+                      <h4 className="title-discription-project">{ project.name }</h4>
+                      <span><strong>{ project.technologies }</strong></span>
+                      <p>
+                        { project.description }
+                      </p>
                     </section>
-                </article>
+                  </li>
+                ))}
+              </ul>
             </section>
-        </>
-    )
+            <section className="container-img-and-skils">
+              <div className="img-about">
+                <img src={imgAbout} alt="" />
+              </div>
+            </section>
+          </section>
+        </article>
+      </section>
+    </>
+  );
 }
 
-export default About
+export default About;
--- src/components/Contac.jsx
+++ src/components/Contac.jsx
@@ -1,36 +1,49 @@
-
-
-
-function Contac(){
-    return(
-        <article className="container-contacto">
-            <section className="container-contacto-title">
-              <h4>Contacto</h4>
-            </section>
-            <ul className="container-data-contact">
-              <li className="list-data-contact">
-                <i class='bx bx-envelope' ></i>
-                <div>
-                  <h6>Email:</h6>
-                  <span>junitrapa357@gmail.com</span>
-                </div>
-              </li>
-              <li className="list-data-contact">
-                <i class='bx bxl-whatsapp' ></i>
-                <div>
-                  <h6>WhatsApp:</h6>
-                  <span>943 413 353</span>
-                </div>
-              </li>
-            </ul>
-            <form className="form-contact">
-              <input placeholder="Nombre" className="input-contact-email" type="text" />
-              <input placeholder="Tu Email" className="input-contact-email" type="text" />
-              <textarea className="textarea-contact" name="" id="" cols="30" rows="10"></textarea>
-              <button className="button-contact">enviar correo</button>
-            </form>
-        </article>
-    )
+function Contac() {
+  return (
+    <article className="container-contacto">
+      <section className="container-contacto-title">
+        <h4>Contacto</h4>
+      </section>
+      <section className="container-data-form">
+        <ul className="container-data-contact">
+          <li className="list-data-contact">
+            <i class="bx bx-envelope"></i>
+            <div>
+              <h6>Email:</h6>
+              <span>junitrapa357@gmail.com</span>
+            </div>
+          </li>
+          <li className="list-data-contact">
+            <i class="bx bxl-whatsapp"></i>
+            <div>
+              <h6>WhatsApp:</h6>
+              <span>943 413 353</span>
+            </div>
+          </li>
+        </ul>
+        <form className="form-contact">
+          <input
+            placeholder="Nombre"
+            className="input-contact-email"
+            type="text"
+          />
+          <input
+            placeholder="Tu Email"
+            className="input-contact-email"
+            type="text"
+          />
+          <textarea
+            className="textarea-contact"
+            name=""
+            id=""
+            cols="60"
+            rows="10"
+          ></textarea>
+          <button className="button-contact">enviar correo</button>
+        </form>
+      </section>
+    </article>
+  );
 }
 
-export default Contac
+export default Contac;
